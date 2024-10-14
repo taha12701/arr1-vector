112c402a456d16c77205f6a16a22150b0f8596cf
taha12701 <108870371+taha12701@users.noreply.github.com>
2024-10-14 23:55:28 +0500

    Add files via upload

---
+++ vec1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// methods to initalize vectors
+int main(){
+  vector<int>vec={1,2,3,4};
+  cout<<vec[1]<<endl;
+
+  vector<int>vec2(3,0);
+  cout<<vec2[1];
+
+
+}
