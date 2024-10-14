36221cadc9eab08ea5da3224b70545ccec78b464
taha12701 <108870371+taha12701@users.noreply.github.com>
2024-10-15 00:55:01 +0500

    Add files via upload

---
+++ vec2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+// vector functions
+int main(){
+
+  // size function
+  vector <char>charac={'a','b','c','d','e'};
+  cout<<"size -> "<<charac.size()<<endl;
+
+  // push back function
+  vector <int>vec;;
+  cout<<"size before push back -> "<<vec.size()<<endl;
+  vec.push_back(25);
+  vec.push_back(35);
+  vec.push_back(65);
+  cout<<"size after push back -> "<<vec.size()<<endl;
+
+
+  cout<<vec.front()<<endl;
+  cout<<vec.at(2);
+
+  // vec.pop_back();
+  // for(int value : vec){
+  //   cout<<value<<" ";
+  }
