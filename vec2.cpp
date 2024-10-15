35abf64ca4f0be98aab7cba442a55aae2e92558d
taha12701 <108870371+taha12701@users.noreply.github.com>
2024-10-15 07:30:54 +0500

    Add files via upload

--- vec2.cpp
+++ vec2.cpp
@@ -7,22 +7,28 @@ using namespace std;
 int main(){
 
   // size function
-  vector <char>charac={'a','b','c','d','e'};
-  cout<<"size -> "<<charac.size()<<endl;
+  // vector <char>charac={'a','b','c','d','e'};
+  // cout<<"size -> "<<charac.size()<<endl;
 
-  // push back function
-  vector <int>vec;;
-  cout<<"size before push back -> "<<vec.size()<<endl;
-  vec.push_back(25);
-  vec.push_back(35);
-  vec.push_back(65);
-  cout<<"size after push back -> "<<vec.size()<<endl;
+  // // push back function
+  // vector <int>vec;;
+  // cout<<"size before push back -> "<<vec.size()<<endl;
+  // vec.push_back(25);
+  // vec.push_back(35);
+  // vec.push_back(65);
+  // cout<<"size after push back -> "<<vec.size()<<endl;
 
 
-  cout<<vec.front()<<endl;
-  cout<<vec.at(2);
+  // cout<<vec.front()<<endl;
+  // cout<<vec.at(2)<<endl;
+  // cout<<vec.empty()<<endl;
 
   // vec.pop_back();
   // for(int value : vec){
   //   cout<<value<<" ";
+
+  // vector<int>vec(5,0);
+  // for(int value : vec){
+  //   cout<<value<<" ";
   }
+  
